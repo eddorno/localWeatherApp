f6de0f8581277399b13cbe818b1f3ebcfd316ee8
eddorno <edjdorno@yahoo.com>
2018-02-14 11:42:08 -0500

    Changed variable names and slight design changes

--- weather.js
+++ weather.js
@@ -8,40 +8,37 @@ $(document).ready(function(){
       $.getJSON("https://api.darksky.net/forecast/f98074bba36ffb96aa44abc641285c53/" + x + "," + y + "?exclude=minutely,hourly,daily,alerts,flags&callback=?", function(json){
         var temp = json.currently.temperature;
         var icon = json.currently.icon;
+        //array of possible icons from API
         var icons = ["clear-day", "clear-night", "rain", "snow", "sleet", "wind", "fog", "cloudy", "partly-cloudy-day", "partly-cloudy-night"];
-        //array of background images
-        var background = ["wi-day-sunny", "wi-night-clear", "wi-rain-mix", "wi-snow", "wi-sleet", "wi-cloudy-windy", "wi-fog", "wi-cloudy", "wi-day-cloudy-high", "wi-night-alt-partly-cloudy"];
+        //array of weather_icons to print to page
+        var weather_icon = ["wi-day-sunny", "wi-night-clear", "wi-rain-mix", "wi-snow", "wi-sleet", "wi-cloudy-windy", "wi-fog", "wi-cloudy", "wi-day-cloudy-high", "wi-night-alt-partly-cloudy"];
+        //define the current weather
         var summary = json.currently.summary;
         //icon = "clear-day"; /*test case*/
         var html = "";
-        //match current icon with location in icons array to find correct background image (ex. icons[2] = background[2] etc.)
+        //match current icon with location in icons array to find correct weather_icon (ex. icons[2] => weather_icon[2] etc.)
         for (var i = 0; i < icons.length; i++){
           if (icon === icons[i]){
-            //console.log(background[i]);
-            //change icon to match weather
+            //change weather_icon to match weather
             //<i class="wi wi-day-sunny"></i>
-            html += "<i class=\"wi wi-fw " + background[i] + "\"></i>";
+            html += "<i class=\"wi " + weather_icon[i] + "\"></i>";
+            //console.log(icon);
+            //console.log(weather_icon[i]);
             //console.log(html);
-            /*html += "<i class='wi '>\"";
-            html += background[i] + ></i>
-            html += ""*/
-            $(".icon").html(html);
-            console.log(background[i]);
-            console.log(html);
+            $(".weather_icon").html(html);
           }
         }
         //get temp in F and C for later use
         var temp_in_fahrenheit = Math.round(temp) + " &#8457";
         var temp_in_celsius = Math.round(((temp - 32) * 5 / 9)) + " &#8451";
-        console.log(icon);
         //console.log(temp_in_celsius);
         //page will load with temp in F already visible and discription of weather
-        $(".here").html(temp_in_fahrenheit);
-        $(".there").html(summary);
+        $(".temp").html(temp_in_fahrenheit);
+        $(".sum").html(summary);
         //quick function to allow switch between F and C with a single click
         var x = 1;
-        $(".here").on("click", function(){
-          (x == 1) ? ($(".here").html(temp_in_celsius), x -= 1) : ($(".here").html(temp_in_fahrenheit), x += 1);
+        $(".temp").on("click", function(){
+          (x == 1) ? ($(".temp").html(temp_in_celsius), x -= 1) : ($(".temp").html(temp_in_fahrenheit), x += 1);
         })
     });
   })
